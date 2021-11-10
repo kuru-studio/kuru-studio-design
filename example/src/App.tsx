bd03acfe4c6d1703ad1a86a1588cf06b4cc43df3
BosEriko <bos.eriko@gmail.com>
2021-11-10 13:38:51 +0800

    Feature: Move to Create React Library

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'kuru-studio-design'
+import 'kuru-studio-design/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
