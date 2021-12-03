c753d7ea3fc797aba1d4b31f9b2dd8c321abc539
BosEriko <bos.eriko@gmail.com>
2021-12-03 20:54:52 +0800

    create a simple example component

---
+++ dist/index.esm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExampleComponent() {
+  return /*#__PURE__*/React.createElement("div", null, "Example!");
+}
+
+export { ExampleComponent as default };
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ExampleComponent() {
+  return (
+    <div>
+      Example!
+    </div>
+  );
+}
