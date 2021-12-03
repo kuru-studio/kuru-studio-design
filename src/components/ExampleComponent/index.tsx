6eca52273f97b7c544e3adaf1d9492cde1f44ab1
BosEriko <bos.eriko@gmail.com>
2021-12-03 22:05:43 +0800

    cleanup

--- src/components/ExampleComponent/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function ExampleComponent() {
-  return (
-    <div>
-      Example!
-    </div>
-  );
-}
--- src/main.tsx
+++
@@ -1,2 +0,0 @@
-export { ExampleComponent } from "./components/ExampleComponent";
-
